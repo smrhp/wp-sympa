436bf7ca03b8d24466a488a6411964c3049f0c88
Stéphane <Stphane@SM-PORTABLE.mshome.net>
2015-11-11 23:23:04 +0100

    début tutos plugin wordpress

--- wp-sympa.php
+++ wp-sympa.php
@@ -1,5 +1,14 @@
 <?php
 /*
 Plugin Name: Wordpress SYMPA
+Plugin URI:  http://plugins.pyrenat.net/wp/wp-sympa
+Description: Interface entre Wordpress et SYMPA
+Version:     0.1
+Author:      Stéphane MUSSET
+Author URI:  http://plugins.pyrenat.net/wp
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: wp-sympa
 */
 
